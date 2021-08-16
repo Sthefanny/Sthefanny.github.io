1cf954ec9c5a61e6c9e3df59c6f833eb91b5ee71
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-08-16 18:42:31 -0300

    testing with initial files

--- vacinada/flutter_service_worker.js
+++ vacinada/flutter_service_worker.js
@@ -3,21 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "02c9eead58452f6c53cd4c7fbcf01d21",
-"index.html": "447c440de2ff438a6c32fc3b9fe0faac",
-"/": "447c440de2ff438a6c32fc3b9fe0faac",
-"main.dart.js": "f521932707c6e99913437956c9153a5c",
+  "version.json": "d1e451799d6b7da0e768b5724d886173",
+"index.html": "32924e18cd350059a8ff8bf6846dfb2b",
+"/": "32924e18cd350059a8ff8bf6846dfb2b",
+"main.dart.js": "29a7d16feb150cea92ef00d69cd9d4ff",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "8e3bf56269fb3b115552547a6460009e",
-"assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"assets/NOTICES": "be20189353564ba3a8bd4c0270930b68",
-"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
+"manifest.json": "15f837639ec8cbfea2c538776e66d308",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/NOTICES": "bde9ee889e7cdb2c908965b81d635c52",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "ffed6899ceb84c60a1efa51c809a57e4",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3241d1d9c15448a4da96df05f3292ffe",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
 };
 
